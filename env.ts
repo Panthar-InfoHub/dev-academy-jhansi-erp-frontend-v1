1062be481aee715b4e2405b928b5bbbd802a6a7d
Raj Dwivedi <rajdwivedipc@gmail.com>
2025-07-14 13:27:06 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 export const BACKEND_SERVER_URL = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL || process.env.BACKEND_SERVER_URL
 
 // School name and other customizable settings
-export const SCHOOL_NAME = process.env.NEXT_PUBLIC_SCHOOL_NAME || process.env.SCHOOL_NAME || "School-ERP"
+export const SCHOOL_NAME = process.env.NEXT_PUBLIC_SCHOOL_NAME || process.env.SCHOOL_NAME
 
 export const CHECK_IN_LAT = process.env.NEXT_PUBLIC_CHECK_IN_LAT || process.env.CHECK_IN_LAT
 export const CHECK_IN_LNG = process.env.NEXT_PUBLIC_CHECK_IN_LNG || process.env.CHECK_IN_LNG
